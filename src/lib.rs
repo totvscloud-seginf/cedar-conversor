58d6c589732f5b44b780d35ca6f90156b0ad3d5a
cleissonbarbosa <cleisson147@gmail.com>
2023-10-31 16:37:37 -0300

    feat: policies to json

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,27 @@ fn echo(s: String) -> PyResult<String> {
 
 #[pyfunction]
 #[pyo3(signature = (policies))]
-fn convert_policies_from_json(policies: String) -> PyResult<String> {
+fn convert_json_to_cedar_policies(policies: String) -> PyResult<String> {
     let policies_json = serde_json::from_str(policies.as_str()).unwrap();
     let conditions = cedar_policy::Policy::from_json(None, policies_json);
     match conditions {
         Ok(conditions) => Ok(conditions.to_string()),
         Err(e) => {
-            println!("❌ error converting conditions from json: {:?}", e);
+            println!("❌ error converting json to cedar policies: {:?}", e);
+            Err(pyo3::exceptions::PyValueError::new_err(e.to_string()))
+        }
+    }
+}
+
+#[pyfunction]
+#[pyo3(signature = (policies))]
+fn convert_cedar_policies_to_json(policies: String) -> PyResult<String> {
+    let policies_json = serde_json::from_str(policies.as_str()).unwrap();
+    let conditions = cedar_policy::Policy::from_json(None, policies_json);
+    match conditions {
+        Ok(conditions) => Ok(conditions.to_string()),
+        Err(e) => {
+            println!("❌ error converting policies to json: {:?}", e);
             Err(pyo3::exceptions::PyValueError::new_err(e.to_string()))
         }
     }
@@ -25,6 +39,6 @@ fn convert_policies_from_json(policies: String) -> PyResult<String> {
 #[pymodule]
 fn _internal(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(echo, m)?)?;
-    m.add_function(wrap_pyfunction!(convert_policies_from_json, m)?)?;
+    m.add_function(wrap_pyfunction!(convert_json_to_cedar_policies, m)?)?;
     Ok(())
 }
